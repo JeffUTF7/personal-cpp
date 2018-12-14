d59e61ee9f0382af307183391b35b41c59982822
JeffUTF8 <jeffutf8@gmail.com>
2018-12-14 14:30:22 +0800

    Modify filtering_program.cpp - add ability to add custom disliked words

--- src/filtering_program.cpp
+++ src/filtering_program.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <limits>
 
 /* A rewritten version of the filtering that was supposed to work, a decade ago.
-   The words in the disliked_words vector is pre-defined, with no choice of control
-   for the user at all. (for now) */
+   The words in the disliked_words vector is (no longer)pre-defined, now with the 
+   ability to define those words yourself. */
 
 int main() {
 
@@ -16,14 +17,20 @@ int main() {
     std::cout << "Enter a list of words: ";
     
     //as long as there are words in the input stream, put it into temporary
-    for (std::string temporary; std::cin >> temporary;) {
+    for (std::string temporary; (std::cin >> temporary) && (temporary != "|");) {
         list_of_words.push_back(temporary);
     }
 
     std::cout << std::endl;
 
     //list of disliked words
-    std::vector<std::string> disliked_words = {"black-clouds", "crackerhound"};
+    std::vector<std::string> disliked_words;
+
+    std::cout << "Enter a list of disliked words: ";    
+
+    for (std::string temporary_disliked_words; (std::cin >> temporary_disliked_words) && (temporary_disliked_words != "|");) {
+        disliked_words.push_back(temporary_disliked_words);
+    }
 
     //cycle through list_of_words and find if it matches any words from
     //disliked_words
